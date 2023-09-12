482487532aaebcabd58f25af64521476ae0e1cec
Santosh <sant43210@gmail.com>
2023-09-12 16:31:03 -0400

    upates on frontend

--- server.py
+++ server.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/index.html')
-def hello_world():
-    return render_template('./index.html')
+@app.route('/')
+def my_home():
+    return render_template('home.html')
 
-@app.route('/elements.html')
-def blog():
-    return render_template('./elements.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/generic.html')
-def blog2():
-    return render_template('./generic.html')
+
+
+#Manual step by step code below for one step dynamic function above
+
+# @app.route('/index.html')
+# def my_home2():
+#     return render_template('index.html')
+
+# @app.route('/elements.html')
+# def blog():
+#     return render_template('elements.html')
+
+# @app.route('/generic.html')
+# def blog2():
+#     return render_template('generic.html')

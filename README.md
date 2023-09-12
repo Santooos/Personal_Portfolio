adba7c2bbfa7477f59b090d8f874f1b4ce707a76
Santosh <sant43210@gmail.com>
2023-09-11 22:09:36 -0400

    Updated path

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Personal_Portfolio
-A details portfolio about me and my journey towards Software Engineering
+A detailed portfolio about me and my journey in Tech.
--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index.html')
 def hello_world():
     return render_template('./index.html')
 
-@app.route('/blog')
+@app.route('/elements.html')
 def blog():
-    return 'These are my thoughts on blog.'
+    return render_template('./elements.html')
+
+@app.route('/generic.html')
+def blog2():
+    return render_template('./generic.html')

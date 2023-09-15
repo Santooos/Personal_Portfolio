24f77884276e152b65a524754bf2728dd0cf3ac3
Santosh <sant43210@gmail.com>
2023-09-14 23:38:05 -0400

    Updates

--- server.py
+++ server.py
@@ -16,12 +16,13 @@ def write_to_file(data):
 
 def write_to_csv(data):
     with open('database.csv', newline = '', mode='a') as database2:
+        name = data["name"]
         email = data["email"]
-        subject = data["subject"]
+        # subject = data["subject"]
         message = data["message"]
         
         csv_writer = csv.writer(database2, delimiter=',', quotechar='"',quoting = csv.QUOTE_MINIMAL)
-        csv_writer.writerow([email,subject,message])
+        csv_writer.writerow([name,email,message])
 
 
 @app.route('/<string:page_name>')
